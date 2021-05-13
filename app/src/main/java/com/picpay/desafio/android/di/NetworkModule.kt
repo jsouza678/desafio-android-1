c32c6f91f9fd206d93acc8bc2a5e206dbba5ad76
Souza <jsouza678@icloud.com>
2021-05-13 16:46:01 -0300

    chore: added network module to koin, and unit tests as well

---
+++ app/src/main/java/com/picpay/desafio/android/di/NetworkModule.kt
@@ -0,0 +1,43 @@
+package com.picpay.desafio.android.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.picpay.desafio.android.PicPayService
+import com.picpay.desafio.android.utils.Constants.BASE_URL
+import com.picpay.desafio.android.utils.Constants.DEFAULT_TIMEOUT
+import org.koin.dsl.module
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+
+    single {
+        val okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+
+        okHttpClient.build()
+    }
+
+    single {
+        GsonBuilder().create()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get<OkHttpClient>())
+            .addConverterFactory(GsonConverterFactory.create(get<Gson>()))
+            .build()
+    }
+
+    single {
+        getPicPayService(get<Retrofit>())
+    }
+}
+
+private fun getPicPayService(retrofit: Retrofit): PicPayService =
+    retrofit.create(PicPayService::class.java)
---
+++ app/src/test/java/com/picpay/desafio/android/di/NetworkModuleTest.kt
@@ -0,0 +1,62 @@
+package com.picpay.desafio.android.di
+
+import com.google.gson.Gson
+import com.picpay.desafio.android.PicPayService
+import com.picpay.desafio.android.utils.Constants
+import org.junit.After
+import org.junit.Before
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import okhttp3.OkHttpClient
+import org.junit.Assert
+import org.junit.Test
+import retrofit2.Retrofit
+
+class NetworkModuleTest : KoinTest {
+
+    private val retrofit: Retrofit by inject()
+    private val okHttpClient: OkHttpClient by inject()
+    private val gson: Gson by inject()
+    private val service: PicPayService by inject()
+
+    @Before
+    fun setup() {
+        startKoin {
+            modules(networkModule)
+        }
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun `WHEN koin application is started it SHOULD instantiate Retrofit`() {
+        Assert.assertNotNull(retrofit)
+    }
+
+    @Test
+    fun `WHEN koin application is started it SHOULD instantiate PicPayService`() {
+        Assert.assertNotNull(service)
+    }
+
+    @Test
+    fun `WHEN koin application is started it SHOULD instantiate Gson`() {
+        Assert.assertNotNull(gson)
+    }
+
+    @Test
+    fun `WHEN koin application is started it SHOULD instantiate OkHttpClient`() {
+        Assert.assertNotNull(okHttpClient)
+    }
+
+    @Test
+    fun `WHEN okHttpClient is instantiated it SHOULD have connect, write and read timeouts set with correct values`() {
+        Assert.assertTrue(okHttpClient.connectTimeoutMillis == (Constants.DEFAULT_TIMEOUT * 1000).toInt())
+        Assert.assertTrue(okHttpClient.readTimeoutMillis == (Constants.DEFAULT_TIMEOUT * 1000).toInt())
+        Assert.assertTrue(okHttpClient.writeTimeoutMillis == (Constants.DEFAULT_TIMEOUT * 1000).toInt())
+    }
+}
