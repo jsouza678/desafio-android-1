665ac92de2ddedbb97939aaf73a35bf6d9e4ba26
Souza <jsouza678@icloud.com>
2021-05-15 14:02:12 -0300

    fix: fixing mapper to prevent crashing when receiving null values. adjusted unit test too.

--- app/src/main/java/com/picpay/desafio/android/data/contacts/data/local/entity/UserLocal.kt
+++ app/src/main/java/com/picpay/desafio/android/data/contacts/data/local/entity/UserLocal.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class UserLocal(
     @PrimaryKey val id: Int,
-    val img: String?,
+    val img: String,
     val name: String,
     val username: String
 )
