bf392f7a334a15dd65bc6c4b87f916c5192a266e
Souza <jsouza678@icloud.com>
2021-05-15 13:08:45 -0300

    fix: putting the correct repository on remote di module

--- app/src/remote/java/com/picpay/desafio/android/data/di/ContactsModule.kt
+++ app/src/remote/java/com/picpay/desafio/android/data/di/ContactsModule.kt
@@ -2,14 +2,14 @@ package com.picpay.desafio.android.data.di
 
 import com.picpay.desafio.android.data.contacts.data.local.dao.ContactsDao
 import com.picpay.desafio.android.data.contacts.data.remote.PicPayService
-import com.picpay.desafio.android.data.contacts.data.repository.AgendaRepositoryMockImpl
+import com.picpay.desafio.android.data.contacts.data.repository.AgendaRepositoryImpl
 import com.picpay.desafio.android.domain.repository.AgendaRepository
 import org.koin.dsl.module
 
 val contactsModule = module {
 
     single {
-        AgendaRepositoryMockImpl(
+        AgendaRepositoryImpl(
             service = get<PicPayService>(),
             dao = get<ContactsDao>()
         ) as AgendaRepository
