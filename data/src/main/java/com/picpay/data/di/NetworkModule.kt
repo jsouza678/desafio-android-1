7d7c7b2ec6596c0653dab7b6bca7edfbc55cef7f
Souza <jsouza678@icloud.com>
2021-05-17 18:54:12 -0300

    chore: adding interceptor to be able to check for request logs on logcat

--- data/src/main/java/com/picpay/data/di/NetworkModule.kt
+++ data/src/main/java/com/picpay/data/di/NetworkModule.kt
@@ -5,8 +5,13 @@ import com.google.gson.GsonBuilder
 import com.picpay.data.contacts.data.remote.PicPayService
 import com.picpay.data.contacts.data.utils.Constants.BASE_URL
 import com.picpay.data.contacts.data.utils.Constants.DEFAULT_TIMEOUT
+import com.picpay.data.utils.Constants.INTERCEPTOR_INSTANCE
+import com.picpay.data.utils.Constants.OKHTTP_INSTANCE
+import com.picpay.data.utils.Constants.RETROFIT_INSTANCE
 import java.util.concurrent.TimeUnit
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,31 +19,38 @@ import retrofit2.converter.gson.GsonConverterFactory
 @Suppress("RemoveExplicitTypeArguments", "USELESS_CAST")
 val networkModule = module {
 
-    single {
+    single(named(OKHTTP_INSTANCE)) {
         val okHttpClient = OkHttpClient().newBuilder()
             .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
             .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
             .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
 
+        okHttpClient.addInterceptor(get<HttpLoggingInterceptor>(named(INTERCEPTOR_INSTANCE)))
         okHttpClient.build()
+    }
+
+    single(named(INTERCEPTOR_INSTANCE)) {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        interceptor
     }
 
     single {
         GsonBuilder().create()
     }
 
-    single {
+    single(named(RETROFIT_INSTANCE)) {
         val baseUrl = getProperty(BASE_URL)
 
         Retrofit.Builder()
             .baseUrl(baseUrl)
-            .client(get<OkHttpClient>())
+            .client(get<OkHttpClient>(named(OKHTTP_INSTANCE)))
             .addConverterFactory(GsonConverterFactory.create(get<Gson>()))
             .build()
     }
 
     single {
-        getPicPayService(get<Retrofit>())
+        getPicPayService(get<Retrofit>(named(RETROFIT_INSTANCE)))
     }
 }
 
